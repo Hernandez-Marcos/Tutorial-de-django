9af302ebdf9835d4aeb69fdf8fce6f4091fa72cc
SgtFreddy713 <captesxel@gmail.com>
2025-12-03 05:36:13 -0300

    now "search for topic" searchbar works in topics.html.

--- base/views.py
+++ base/views.py
@@ -196,5 +196,7 @@ def updateUser(request):
 
 
 def topicsPage(request):
-    topics = Topic.objects.filter()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html', {'topics': topics})
